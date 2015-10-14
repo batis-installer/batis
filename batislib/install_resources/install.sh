db0efb35dfd5ee78981f4c8cff8f0079f7fbed9b
Thomas Kluyver <takowl@gmail.com>
2015-10-14 11:53:43 +0100

    Nicer shell syntax for picking Python to run with

--- batislib/install_resources/install.sh
+++ batislib/install_resources/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 appdir=`dirname $0`
 # Run the Python installer script with python3 if it exists, python if not
-which python3 && python3 $appdir/batis_info/selfinstall.py $appdir \
-              || python  $appdir/batis_info/selfinstall.py $appdir
+if which python3; then
+    python3 $appdir/batis_info/selfinstall.py $appdir
+else
+    python  $appdir/batis_info/selfinstall.py $appdir
+fi
